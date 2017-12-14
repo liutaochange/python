1958d2c6bbdccd0b888c63602fe58ab31bd0171c
liutaochange <liutaochange@163.com>
2017-12-14 18:27:06 +0800

    功能实现

---
+++ README.md
@@ -0,0 +1,11 @@
+
+# 爬取微信好友头像，并拼接成一张图片
+
+
+
+### clone 代码到本地，执行
+
+>  pip install itchat   
+>  pip install pillow
+
+### 然后到项目目录下 执行 python search.py  会出现二维码，用微信扫一扫获取用户信息，就开始去获取微信好友头像
---
+++ search.py
@@ -0,0 +1,39 @@
+import itchat
+import math
+import PIL.Image as Image
+import os
+
+itchat.auto_login()
+friends = itchat.get_friends(update=True)[0:]
+user = friends[0]["UserName"]
+
+num = 0
+for i in friends:
+    img = itchat.get_head_img(userName=i["UserName"])
+    fileImage = open('./saveImg' + "/" + str(num) + ".jpg",'wb')
+    fileImage.write(img)
+    fileImage.close()
+    num += 1
+
+ls = os.listdir('./saveImg')
+each_size = int(math.sqrt(float(640*640)/len(ls)))
+lines = int(640/each_size)
+image = Image.new('RGB', (640, 640))
+x = 0
+y = 0
+for i in range(0,len(ls)+1):
+    try:
+        img = Image.open('./saveImg' + "/" + str(i) + ".jpg")
+        if img.mode != "RGB":
+            img = img.convert("RGB")
+    except IOError:
+        print("Error")
+    else:
+        img = img.resize((each_size, each_size), Image.ANTIALIAS)
+        image.paste(img, (x * each_size, y * each_size))
+        x += 1
+        if x == lines:
+            x = 0
+            y += 1
+image.save('./saveImg' + "/" + "all.jpg")
+itchat.send_image('./saveImg' + "/" + "all.jpg", 'filehelper')
